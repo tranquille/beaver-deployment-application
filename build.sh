0a650550b530ea7a8c257aa2cf334065a4691a55
Arkadiusz Czarnik <arkadiusz.czarnik@obi.de>
2018-03-20 12:46:59 +0100

    prometheus service added.

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+helm lint charts/*
+for chart in charts/*; do helm dep up $chart; done
+helm package -d docs charts/*
+helm repo index docs --url https://github.com/WildBeavers/beaver-deployment-application
