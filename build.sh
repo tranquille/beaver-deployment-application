d9f1d80190c0cfbc8959765520b8cbf1e3e51724
Arkadiusz Czarnik <arkadiusz.czarnik@obi.de>
2018-03-20 13:36:45 +0100

    add docs for helm

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 helm lint charts/*
 for chart in charts/*; do helm dep up $chart; done
 helm package -d docs charts/*
-helm repo index docs --url https://github.com/WildBeavers/beaver-deployment-application/
+helm repo index docs --url https://wildbeavers.github.io/beaver-deployment-application/
